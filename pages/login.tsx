8c5f67cb115126fa529fd2fdf0ed90d8f68f15c0
Ronak Chatterjee <ronak.chatterjee65@gmail.com>
2023-05-14 19:41:58 +0100

    boilerplate

---
+++ pages/login.tsx
@@ -0,0 +1,64 @@
+import {
+  Button,
+  Checkbox,
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import { Logo } from "../components/Logo";
+import IndexPageNavbar from "@/components/IndexPageNavbar";
+
+const Login = () => (
+  <>
+    <IndexPageNavbar />
+    <Container maxW="md" py={{ base: "12", md: "24" }}>
+      <Stack spacing="8">
+        <Stack spacing="6">
+          <Logo />
+          <Stack spacing={{ base: "2", md: "3" }} textAlign="center">
+            <Heading size={{ base: "xs", md: "sm" }}>
+              Log in to your account
+            </Heading>
+            <Text color="muted">Start making your dreams come true</Text>
+          </Stack>
+        </Stack>
+        <Stack spacing="6">
+          <Stack spacing="5">
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <Input id="email" placeholder="Enter your email" type="email" />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input id="password" placeholder="********" type="password" />
+            </FormControl>
+          </Stack>
+          <HStack justify="space-between">
+            <Checkbox defaultChecked>Remember me</Checkbox>
+            <Button variant="link" colorScheme="blue" size="sm">
+              Forgot password
+            </Button>
+          </HStack>
+          <Stack spacing="4">
+            <Button>Sign in</Button>
+          </Stack>
+        </Stack>
+        <HStack spacing="1" justify="center">
+          <Text fontSize="sm" color="muted">
+            Don't have an account?
+          </Text>
+          <Button variant="link" colorScheme="blue" size="sm">
+            Sign up
+          </Button>
+        </HStack>
+      </Stack>
+    </Container>
+  </>
+);
+
+export default Login;
