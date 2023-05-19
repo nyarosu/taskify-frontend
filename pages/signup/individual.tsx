fa5326105febf5de507ba9d4f8114d63df0d2898
Ronak Chatterjee <ronak.chatterjee65@gmail.com>
2023-05-19 22:55:33 +0100

    abc

--- pages/signup/individual.tsx
+++ pages/signup/individual.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import {
+  Alert,
+  AlertIcon,
   Button,
   Container,
   FormControl,
@@ -17,11 +19,18 @@ import { Logo } from "@/components/Logo";
 import { GoogleIcon } from "@/assets/ProviderIcon";
 import IndexPageNavbar from "@/components/IndexPageNavbar";
 import { useRouter } from "next/router";
+import { API_URL } from "../_app";
+import { useAppDispatch } from "@/utils/redux_hooks";
+import { login } from "@/utils/store";
+import { SignupModes } from "@/utils/SignUpModes";
 
 const validationSchema = Yup.object().shape({
-  name: Yup.string()
-    .required("Name is required")
-    .matches(/^[a-zA-Z\s]*$/, "Name must not contain numbers"),
+  firstName: Yup.string()
+    .required("First name is required")
+    .matches(/^[a-zA-Z\s]*$/, "First name must not contain numbers"),
+  lastName: Yup.string()
+    .required("Last name is required")
+    .matches(/^[a-zA-Z\s]*$/, "Last name must not contain numbers"),
   email: Yup.string().required("Email is required").email("Invalid email"),
   password: Yup.string()
     .required("Password is required")
@@ -29,11 +38,15 @@ const validationSchema = Yup.object().shape({
 });
 
 const IndividualSignup = () => {
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [registerError, setRegisterError] = useState(false);
+  const dispatcher = useAppDispatch();
+
   const router = useRouter();
   const formik = useFormik({
     initialValues: {
-      name: "",
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
     },
@@ -41,7 +54,40 @@ const IndividualSignup = () => {
     onSubmit: handleSubmit,
   });
 
-  function handleSubmit(values: any) {}
+  async function handleSubmit(values: any) {
+    setIsRegistering(true);
+    setRegisterError(false);
+    const response = await fetch(`${API_URL}/auth/signup`, {
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      method: "POST",
+      body: JSON.stringify({
+        mode: SignupModes.Individual,
+        first_name: values.firstName,
+        last_name: values.lastName,
+        email: values.email,
+        password: values.password,
+      }),
+    });
+    console.log(response);
+
+    if (response.ok) {
+      const json = await response.json();
+      dispatcher(
+        login({
+          first_name: json.first_name,
+          last_name: json.last_name,
+          email: json.email,
+          company: json.company_name,
+          picture: "" /* TODO */,
+        })
+      );
+      router.push("/dashboard", undefined, { shallow: true });
+    } else {
+      setIsRegistering(false);
+      setRegisterError(true);
+    }
+  }
 
   return (
     <>
@@ -50,6 +96,12 @@ const IndividualSignup = () => {
       <Container maxW="md" py={{ base: "12", md: "24" }}>
         <Stack spacing="8">
           <Stack spacing="6" align="center">
+            {registerError && (
+              <Alert status="error">
+                <AlertIcon />
+                User already exists.
+              </Alert>
+            )}
             <Logo boxSize="4rem" />
             <Stack spacing="3" textAlign="center">
               <Heading size={{ base: "xs", md: "sm" }}>
@@ -59,51 +111,65 @@ const IndividualSignup = () => {
           </Stack>
           <form onSubmit={formik.handleSubmit}>
             <Stack spacing="6">
-              <Stack spacing="5">
+              <Stack spacing="5" direction={{ base: "column", md: "row" }}>
                 <FormControl
                   isRequired
-                  isInvalid={!!formik.errors.name && formik.touched.name}
+                  isInvalid={
+                    !!formik.errors.firstName && formik.touched.firstName
+                  }
                 >
-                  <FormLabel htmlFor="name">Name</FormLabel>
+                  <FormLabel htmlFor="firstName">First name</FormLabel>
                   <Input
-                    id="name"
+                    id="firstName"
                     type="text"
-                    {...formik.getFieldProps("name")}
+                    {...formik.getFieldProps("firstName")}
                   />
-                  <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
-                </FormControl>
-                <FormControl
-                  isRequired
-                  isInvalid={!!formik.errors.email && formik.touched.email}
-                >
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <Input
-                    id="email"
-                    type="email"
-                    {...formik.getFieldProps("email")}
-                  />
-                  <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+                  <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
                 </FormControl>
                 <FormControl
                   isRequired
                   isInvalid={
-                    !!formik.errors.password && formik.touched.password
+                    !!formik.errors.lastName && formik.touched.lastName
                   }
                 >
-                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <FormLabel htmlFor="lastName">Last name</FormLabel>
                   <Input
-                    id="password"
-                    type="password"
-                    {...formik.getFieldProps("password")}
+                    id="lastName"
+                    type="text"
+                    {...formik.getFieldProps("lastName")}
                   />
-                  <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+                  <FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
                 </FormControl>
               </Stack>
+              <FormControl
+                isRequired
+                isInvalid={!!formik.errors.email && formik.touched.email}
+              >
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input
+                  id="email"
+                  type="email"
+                  {...formik.getFieldProps("email")}
+                />
+                <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+              </FormControl>
+              <FormControl
+                isRequired
+                isInvalid={!!formik.errors.password && formik.touched.password}
+              >
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  {...formik.getFieldProps("password")}
+                />
+                <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+              </FormControl>
               <Stack spacing="4">
                 <Button
                   type="submit"
                   variant="primary"
-                  isLoading={isSubmitting}
+                  isLoading={isRegistering}
                   loadingText="Creating account"
                 >
                   Create account
@@ -118,7 +184,7 @@ const IndividualSignup = () => {
               </Stack>
             </Stack>
           </form>
-          <HStack justify="center" spacing="1">
+          <HStack justify="center" spacing="1" mt={4}>
             <Text fontSize="sm" color="muted">
               Already have an account?
             </Text>
---
+++ utils/SignupModes.tsx
@@ -0,0 +1,4 @@
+export enum SignupModes {
+  Individual = "Individual",
+  Organization = "Organization",
+}
