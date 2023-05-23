e18859e53936f227b55b7dc2182d1fef75495f87
Ronak Chatterjee <ronak.chatterjee65@gmail.com>
2023-05-23 09:44:11 +0100

    abcd

--- components/InviteUserModal.tsx
+++ components/InviteUserModal.tsx
@@ -18,17 +18,21 @@ import {
   VStack,
   Text,
   HStack,
+  useToast,
 } from "@chakra-ui/react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { MdPerson } from "react-icons/md";
 import { BsFillPersonPlusFill } from "react-icons/bs";
+import { API_URL } from "@/pages/_app";
 
 const InviteUserModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
   isOpen,
   onClose,
 }) => {
   const [selectedRole, setSelectedRole] = useState("");
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   const handleRoleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedRole(event.target.value);
@@ -41,8 +45,63 @@ const InviteUserModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
     role: Yup.string().required("Role is required"),
   });
 
-  const handleSubmit = (values: any) => {
-    console.log("Form Values:", values);
+  const handleSubmit = async (values: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: string;
+  }) => {
+    setLoading(true);
+
+    const response = await fetch(`${API_URL}/company/invite`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        first_name: values.firstName,
+        last_name: values.lastName,
+        email: values.email,
+        role: values.role,
+      }),
+    });
+
+    setLoading(false);
+
+    if (!response.ok) {
+      if (response.status === 409) {
+        toast({
+          title: "An error occurred.",
+          position: "top",
+          description:
+            "The user you're trying to invite already has an account with this email.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "An error occurred.",
+          position: "top",
+          description: "An unknown error occurred. Please try again later.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    } else {
+      toast({
+        title: "Invite sent!",
+        position: "top",
+        description:
+          "An invite has been sent to the email address you specified.",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+
     onClose();
   };
 
@@ -66,10 +125,13 @@ const InviteUserModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
               role: "",
             }}
             validationSchema={inviteUserSchema}
-            onSubmit={handleSubmit}
+            onSubmit={(values, { resetForm }) => {
+              handleSubmit(values);
+              resetForm();
+            }}
           >
-            {({ values, errors, touched }) => (
-              <Form>
+            {({ values, errors, touched, handleSubmit }) => (
+              <Form onSubmit={handleSubmit}>
                 <VStack spacing={4} align="stretch">
                   <Grid templateColumns="repeat(2, 1fr)" gap={4}>
                     <FormControl
@@ -77,11 +139,14 @@ const InviteUserModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
                     >
                       <FormLabel>First Name</FormLabel>
                       <Field as={Input} name="firstName" />
-                      {errors.firstName && touched.firstName && (
-                        <Text color="red.500" fontSize="sm">
-                          {errors.firstName}
-                        </Text>
-                      )}
+                      <ErrorMessage
+                        name="firstName"
+                        render={(msg) => (
+                          <Text color="red.500" fontSize="sm">
+                            {msg}
+                          </Text>
+                        )}
+                      />
                     </FormControl>
 
                     <FormControl
@@ -89,47 +154,55 @@ const InviteUserModal: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
                     >
                       <FormLabel>Last Name</FormLabel>
                       <Field as={Input} name="lastName" />
-                      {errors.lastName && touched.lastName && (
-                        <Text color="red.500" fontSize="sm">
-                          {errors.lastName}
-                        </Text>
-                      )}
+                      <ErrorMessage
+                        name="lastName"
+                        render={(msg) => (
+                          <Text color="red.500" fontSize="sm">
+                            {msg}
+                          </Text>
+                        )}
+                      />
                     </FormControl>
                   </Grid>
 
                   <FormControl isInvalid={!!errors.email && touched.email}>
                     <FormLabel>Email</FormLabel>
                     <Field as={Input} name="email" />
-                    {errors.email && touched.email && (
-                      <Text color="red.500" fontSize="sm">
-                        {errors.email}
-                      </Text>
-                    )}
+                    <ErrorMessage
+                      name="email"
+                      render={(msg) => (
+                        <Text color="red.500" fontSize="sm">
+                          {msg}
+                        </Text>
+                      )}
+                    />
                   </FormControl>
 
                   <FormControl isInvalid={!!errors.role && touched.role}>
                     <FormLabel>Role</FormLabel>
                     <Field as={Input} name="role" />
-                    {errors.role && touched.role && (
-                      <Text color="red.500" fontSize="sm">
-                        {errors.role}
-                      </Text>
-                    )}
+                    <ErrorMessage
+                      name="role"
+                      render={(msg) => (
+                        <Text color="red.500" fontSize="sm">
+                          {msg}
+                        </Text>
+                      )}
+                    />
                   </FormControl>
                 </VStack>
+                <ModalFooter>
+                  <Button variant="ghost" mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                  <Button colorScheme="blue" type="submit" isLoading={loading}>
+                    Invite User
+                  </Button>
+                </ModalFooter>
               </Form>
             )}
           </Formik>
         </ModalBody>
-
-        <ModalFooter>
-          <Button variant="ghost" mr={3} onClick={onClose}>
-            Close
-          </Button>
-          <Button colorScheme="blue" type="submit" form="invite-user-form">
-            Invite User
-          </Button>
-        </ModalFooter>
       </ModalContent>
     </Modal>
   );
