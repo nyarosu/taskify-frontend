00bc7821d7dbf3bb68d61fe8ee20cedf55aa39ad
Ronak Chatterjee <ronak.chatterjee65@gmail.com>
2023-05-30 23:20:10 +0100

    abcd

--- components/ProjectsPageHeader.tsx
+++ components/ProjectsPageHeader.tsx
@@ -13,35 +13,45 @@ import {
 } from "@chakra-ui/react";
 import { FiSearch } from "react-icons/fi";
 import { AiOutlineFundProjectionScreen, AiOutlinePlus } from "react-icons/ai";
+import { useAppSelector } from "@/utils/redux_hooks";
 
 export const ProjectsPageHeader: React.FC<{ hasProjects: boolean }> = (
   props
-) => (
-  <Container>
-    <Stack
-      spacing="4"
-      direction={{ base: "column", md: "row" }}
-      justify="space-between"
-    >
-      <HStack spacing="3">
-        <AiOutlineFundProjectionScreen size="1.8rem" />
-        <Heading size={{ base: "xs", md: "sm" }} fontWeight="medium">
-          Projects
-        </Heading>
-      </HStack>
-      {props.hasProjects && (
-        <HStack spacing="2.5rem">
-          <InputGroup maxW={{ sm: "xs" }}>
-            <InputLeftElement pointerEvents="none">
-              <Icon as={FiSearch} color="muted" boxSize="5" />
-            </InputLeftElement>
-            <Input placeholder="Search" />
-          </InputGroup>
-          <Button variant="primary" leftIcon={<AiOutlinePlus />} width="6rem">
-            New
-          </Button>
+) => {
+  const user = useAppSelector((state) => state.user);
+  return (
+    <Container>
+      <Stack
+        spacing="4"
+        direction={{ base: "column", md: "row" }}
+        justify="space-between"
+      >
+        <HStack spacing="3">
+          <AiOutlineFundProjectionScreen size="1.8rem" />
+          <Heading size={{ base: "xs", md: "sm" }} fontWeight="medium">
+            Projects
+          </Heading>
         </HStack>
-      )}
-    </Stack>
-  </Container>
-);
+        {props.hasProjects && (
+          <HStack spacing="2.5rem">
+            <InputGroup maxW={{ sm: "xs" }}>
+              <InputLeftElement pointerEvents="none">
+                <Icon as={FiSearch} color="muted" boxSize="5" />
+              </InputLeftElement>
+              <Input placeholder="Search" />
+            </InputGroup>
+            {user.isCompanyAdmin && (
+              <Button
+                variant="primary"
+                leftIcon={<AiOutlinePlus />}
+                width="6rem"
+              >
+                New
+              </Button>
+            )}
+          </HStack>
+        )}
+      </Stack>
+    </Container>
+  );
+};
